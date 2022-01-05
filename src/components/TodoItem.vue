9eee891e1722311b2e15a5b3b67f7b7bac7775bd
Suon Samoun <suonsamoun@gmail.com>
2022-01-05 16:01:50 +0700

    [PhaseFour]: Using Composition Api

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,17 +1,44 @@
 <template>
     <div class="flex items-center justify-between my-2 p-1">
         <div class="flex">
-                <div class="flex-shrink-0 m-1 align-middle">
+            <div class="flex-shrink-0 m-1 align-middle">
                 <input type="checkbox" :checked="completed" @change="toggleCompleted()" />
             </div>
-            <h4 class="text-xl text-gray-900 leading-tight ml-6" :class="completed ? 'line-through' : ''">{{ text }}</h4>
+            <h4
+                class="text-xl text-gray-900 leading-tight ml-6"
+                :class="completed ? 'line-through' : ''"
+            >{{ text }}</h4>
         </div>
         <div class="flex items-center">
-            <svg @click="editItem()" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-600 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+            <svg
+                @click="editItem()"
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 text-green-600 cursor-pointer"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                />
             </svg>
-            <svg @click="deleteItem()" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-3 text-rose-600 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+            <svg
+                @click="deleteItem()"
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 ml-3 text-rose-600 cursor-pointer"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                />
             </svg>
         </div>
     </div>
@@ -20,6 +47,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { MutationTypes } from "@/store/modules/todo/mutations";
+import { useStore } from "@/store";
 
 export default defineComponent({
     name: "TodoItem",
@@ -29,20 +57,28 @@ export default defineComponent({
         completed: { type: Boolean, required: true }
     },
 
-    methods: {
-        toggleCompleted(): void {
-           this.$store.commit(MutationTypes.TOGGLE_COMPLETED, this.$props.id);
-        },
+    setup(props) {
+        const { commit } = useStore();
 
-        editItem(): void {
-            this.$store.commit(MutationTypes.EDIT_ITEM, this.$props);
-        },
+        const toggleCompleted = (): void => {
+            commit(MutationTypes.TOGGLE_COMPLETED, props.id);
+        };
 
-        deleteItem(): void {
-            this.$store.commit(MutationTypes.DELETE_ITEM, this.$props.id);
-        },
-    },
+        const editItem = (): void => {
+            commit(MutationTypes.EDIT_ITEM, props);
+        };
 
+        const deleteItem = (): void => {
+            commit(MutationTypes.DELETE_ITEM, props.id);
+        };
+
+        return {
+            toggleCompleted,
+            editItem,
+            deleteItem
+        };
+
+    }
 });
 
 </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,27 +1,24 @@
 <template>
-    <TodoItem 
-        v-for="item in items" 
-        :key="item.id"
-        v-bind="item" 
-    />
+    <TodoItem v-for="item in items" :key="item.id" v-bind="item" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
 import { TodoItem } from "@/components";
 import { Todo } from "@/interfaces";
+import { useStore } from "@/store";
 
 export default defineComponent({
     name: "TodoList",
     components: {
         TodoItem,
     },
-    
-    computed: {
-        items(): Todo[] {
-            return this.$store.state.todo.items;
-        },
-    },
 
+    setup() {
+        const { state } = useStore();
+        const items = computed((): Todo[] => state.todo.items);
+
+        return { items };
+    }
 });
 </script>
