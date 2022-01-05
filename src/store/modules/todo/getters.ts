77435fa83a9d400665733e0c67a5313dd21f57d2
Suon Samoun <suonsamoun@gmail.com>
2022-01-05 13:01:38 +0700

    [Enhance 0103]: use computed

--- src/store/modules/todo/getters.ts
+++ src/store/modules/todo/getters.ts
@@ -5,6 +5,8 @@ import { RootState } from '@/store';
 export type Getters = {
     totalCount(state: State): number
     completedCount(state: State): number
+    isValid:(state: State) => (text: string) => boolean
+    itemExist:(state: State) => (text: string) => boolean
 }
 
 export const getters: GetterTree<State, RootState> & Getters = {
@@ -13,5 +15,18 @@ export const getters: GetterTree<State, RootState> & Getters = {
     },
     completedCount(state) {
         return state.items.filter(item => item.completed).length;
+    },
+    isValid: state => (text: string) => {
+        return text.trim().length === 0 || getters.itemExist(state)(text);
+    },
+    itemExist: state => (text: string) => {
+        return state.items.some(item => item.text.toLowerCase() === text.toLocaleLowerCase());
     }
+
+    //2nd way
+    // itemExist(state) {
+    //     return (text: string) => {
+    //         return state.items.some(item => item.text.toLowerCase() === text.toLocaleLowerCase());
+    //     }
+    // }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
 			<form class="my-4" @submit.prevent="addNewItem">
 				<div
 					class="mx-auto flex items-center bg-white p-2 rounded-md border-2"
-					:class="[isEmpty ? '' : 'border-green-500', isRequired ? 'border-red-500' : '']"
+					:class="[isValid(text) ? '' : 'border-green-500']"
 				>
 					<div class="flex-grow m-1 ml-3">
 						<input
@@ -21,25 +21,25 @@
 							class="w-full focus:outline-none"
 							type="text"
 							placeholder="Enter new item"
-							@keyup="validateInput"
 						/>
 					</div>
 					<div class="flex-shrink-0">
 						<button
 							type="submit"
 							class="text-white font-bold py-2 px-3 rounded"
-							:class="isEmpty ? 'bg-gray-300' : 'bg-green-500 hover:bg-green-700'"
-							:disabled="isEmpty"
+							:class="isValid(text) ? 'bg-gray-300' : 'bg-green-500 hover:bg-green-700'"
+							:disabled="isValid(text)"
 						>Add</button>
 					</div>
 				</div>
 			</form>
-			<h5 class="text-left text-red-600" v-show="isRequired && !isDuplicated">Please enter an item!</h5>
-			<h5 class="text-left text-yellow-600" v-show="isDuplicated">Item {{ text }} already exists!</h5>
-			<h5
-				class="text-left text-green-500 transition ease-in-out delay-150"
-				v-show="isSuccess"
-			>Item successfully added!</h5>
+			<h5 class="text-left text-red-500 px-2" v-show="hasItemExist(text)">
+				<span>Item <strong>{{ text }}</strong> already exists!</span>
+			</h5>
+			<h5 class="text-left text-green-500 transition ease-in-out delay-150 px-2" v-show="isSuccess">
+				Item successfully added!
+			</h5>
+			<h5 class="text-left text-red-500 px-2" v-show="totalCount == 0">You don't have any item!</h5>
 			<TodoList />
 		</div>
 	</div>
@@ -60,27 +60,19 @@ export default defineComponent({
 	data() {
 		return {
 			text: '',
-			showWelcomeMsg: false,
-			isRequired: false,
 			isSuccess: false,
-			isDuplicated: false,
+			showWelcomeMsg: false,
 		} as {
 			text: string;
-			isRequired: boolean;
 			isSuccess: boolean;
-			isDuplicated: boolean;
 			showWelcomeMsg: boolean;
 		};
 	},
 	methods: {
-		validateInput(): boolean {
-			return this.isRequired = this.isEmpty;
-		},
 
 		async addNewItem(): Promise<void> {
 
-			if (this.isEmpty) {
-				this.isRequired = true;
+			if (this.isValid(this.text)) {
 				this.isSuccess = false;
 				return;
 			}
@@ -95,7 +87,6 @@ export default defineComponent({
 			this.$store.commit(MutationTypes.CREATE_ITEM, item);
 
 			this.isSuccess = true;
-			this.isRequired = false;
 			this.text = "";
 
 			await sleep(100);
@@ -112,17 +103,21 @@ export default defineComponent({
 			return this.$store.state.todo.loading;
 		},
 
-		isEmpty(): boolean {
-			return this.text === '' || this.isDuplicated;
-		},
-
 		completedCount(): number {
 			return this.$store.getters.completedCount;
 		},
 
 		totalCount(): number {
 			return this.$store.getters.totalCount;
-		}
+		},
+
+		isValid() {
+			return this.$store.getters.isValid;
+		},
+
+		hasItemExist() {
+			return this.$store.getters.itemExist;
+		},
 
 	},
 
@@ -134,18 +129,6 @@ export default defineComponent({
 
 	mounted() {
 		this.$store.dispatch(ActionTypes.GET_ITEMS);
-	},
-
-	watch: {
-		text(newValue: string): void {
-			const items: Todo[] = this.$store.state.todo.items;
-			const duplicatedItem = items.find(item => item.text.toLowerCase() === newValue.toLowerCase());
-			if (duplicatedItem) {
-				this.isDuplicated = true;
-			} else {
-				this.isDuplicated = false;
-			}
-		}
 	}
 });
 
